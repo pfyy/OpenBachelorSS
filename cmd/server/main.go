14b7740f8e17069d8378089a62474710e3fab20f
pfyy <112098515+pfyy@users.noreply.github.com>
2025-09-07 00:41:50 +0800

    basic server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net"
+	"os"
 
+	"github.com/OpenBachelor/OpenBachelorSS/internal/session"
+)
+
+func handleConnection(ctx context.Context, conn session.Conn) {
+	defer conn.Close()
+
+	log.Printf("conn: %+v", conn)
+
+	session := session.NewSession(ctx, conn)
+	defer session.Close()
+}
+
+func main() {
+	listenAddress := "127.0.0.1:8453"
+
+	listener, err := net.Listen("tcp", listenAddress)
+	if err != nil {
+		log.Printf("failed to listen: %v", err)
+		os.Exit(1)
+	}
+
+	defer listener.Close()
+
+	for {
+		netConn, err := listener.Accept()
+
+		if err != nil {
+			log.Printf("failed to accept: %v", err)
+			continue
+		}
+
+		conn, ok := netConn.(session.Conn)
+		if !ok {
+			log.Printf("not a valid conn")
+			continue
+		}
+
+		go handleConnection(context.Background(), conn)
+	}
 }
